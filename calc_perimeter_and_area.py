20b6103a2e3f195a276cc1fd60eb9eb5bea19096
Anniel Menezes <annielmenezes@ideiaseo.com>
2017-08-28 17:25:40 -0300

    #1 - Primeiro exercício da lista 1

---
+++ calc_perimeter_and_area.py
@@ -0,0 +1,7 @@
+side_value = int(input('Digite o valor correspondente ao lado de um quadrado: '))
+
+perimeter_square = side_value * 4
+
+area_square = side_value * side_value
+
+print("perímetro: "+ str(perimeter_square) + " - área: " + str(area_square))
